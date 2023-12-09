7f559c2addbf35707fa02f03dcbe94a3b1186420
Shuhei Matsuo <mashooo@g.ecc.u-tokyo.ac.jp>
2023-12-09 14:04:13 +0900

    modified sbatch_run.sh

--- distributed/ddp-tutorial-series/slurm/sbatch_run.sh
+++ distributed/ddp-tutorial-series/slurm/sbatch_run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=multinode-example
-#SBATCH --nodes=2
-#SBATCH --ntasks=2
-#SBATCH --gpus-per-task=1
-#SBATCH --exclusive
+#SBATCH --exclusive --nodes=2 --ntasks-per-node=1 --gpus=2
 #SBATCH --chdir=/home/nml-user/data/pytorch_examples/distributed/ddp-tutorial-series
 
 srun -l --ntasks-per-node=1 slurm/torchrun.sh
