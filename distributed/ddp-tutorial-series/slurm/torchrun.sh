e38e126bfccc99343215b09ae1f7b88cba2f9706
Shuhei Matsuo <mashooo@g.ecc.u-tokyo.ac.jp>
2023-12-09 14:47:15 +0900

    modified torchrun.sh

--- distributed/ddp-tutorial-series/slurm/torchrun.sh
+++ distributed/ddp-tutorial-series/slurm/torchrun.sh
@@ -8,7 +8,7 @@ export LOGLEVEL=INFO
 
 torchrun \
 --nnodes 2 \
---nproc_per_node=$SLURM_GPUS_PER_TASK \
+--nproc_per_node=$SLURM_GPUS_ON_NODE \
 --node_rank=$SLURM_NODEID \
 --master_addr=$1 --master_port=7657 \
 multinode.py 50000 10
