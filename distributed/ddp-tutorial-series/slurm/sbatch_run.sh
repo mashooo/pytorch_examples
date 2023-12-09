01117cd7087dfa2422a9a33e630275ed641f5389
Shuhei Matsuo <mashooo@g.ecc.u-tokyo.ac.jp>
2023-12-09 14:11:06 +0900

    modified sbatch_run.sh and torchrun.sh

--- distributed/ddp-tutorial-series/slurm/sbatch_run.sh
+++ distributed/ddp-tutorial-series/slurm/sbatch_run.sh
@@ -4,4 +4,11 @@
 #SBATCH --exclusive --nodes=2 --ntasks-per-node=1 --gpus=2
 #SBATCH --chdir=/home/nml-user/data/pytorch_examples/distributed/ddp-tutorial-series
 
-srun -l --ntasks-per-node=1 slurm/torchrun.sh
+nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ips=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --all-ip-addresses)
+head_node_ips_array=($head_node_ips)
+head_node_ip=${head_node_ips_array[0]}
+
+srun -l --ntasks-per-node=1 "slurm/torchrun.sh $head_node_ip"
--- distributed/ddp-tutorial-series/slurm/torchrun.sh
+++ distributed/ddp-tutorial-series/slurm/torchrun.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-head_node_ips=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --all-ip-addresses)
-head_node_ips_array=($head_node_ips)
-head_node_ip=${head_node_ips_array[0]}
-
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate qlin
 
-echo Node IP: $head_node_ip
+echo Node IP: $1
 export LOGLEVEL=INFO
 
 srun torchrun \
 --nnodes 2 \
 --nproc_per_node 1 \
---master_addr=$head_node_ip --master_port=7657 \
+--master_addr=$1 --master_port=7657 \
 /shared/examples/multinode_torchrun.py 50 10
