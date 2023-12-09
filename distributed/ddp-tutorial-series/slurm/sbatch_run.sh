091da33786d8568bc02fce501dd1d664680fe0b8
Shuhei Matsuo <mashooo@g.ecc.u-tokyo.ac.jp>
2023-12-09 13:06:49 +0900

    modified sbatch_run.sh

--- distributed/ddp-tutorial-series/slurm/sbatch_run.sh
+++ distributed/ddp-tutorial-series/slurm/sbatch_run.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 #SBATCH --job-name=multinode-example
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=4
 
 nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
 nodes_array=($nodes)
 head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+head_node_ips=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --all-ip-addresses)
+head_node_ips_array=($head_node_ips)
+head_node_ip=${head_node_ips_array[0]}
 
 echo Node IP: $head_node_ip
 export LOGLEVEL=INFO
 
 srun torchrun \
---nnodes 4 \
+--nnodes 2 \
 --nproc_per_node 1 \
 --rdzv_id $RANDOM \
 --rdzv_backend c10d \
